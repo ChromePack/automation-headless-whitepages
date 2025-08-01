f4d78f28bca0ff5c5c62966397f8b20020a796f1
ChromePackDev <developer@chromepack.dev>
2025-08-02 02:14:56 +0400

    Add ecosystem configuration for PM2 deployment
    - Introduced `ecosystem.config.js` to manage application deployment and process management using PM2.
    - Configured production environment settings, including memory limits and restart policies.
    - Set up deployment instructions for production, including repository details and post-deployment commands.
    
    These changes enhance the deployment process and ensure better management of the application in a production environment.

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  apps: [
+    {
+      name: "whitepages-api",
+      script: "yarn",
+      args: "run start:prod",
+      cwd: "./",
+      watch: false,
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        DISPLAY: ":1",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        DISPLAY: ":1",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "31.97.13.50",
+      ref: "origin/master",
+      repo: "https://github.com/ChromePack/automation-headless-whitepages",
+      path: "/var/www/whitepagesapi-headless",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "yarn install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
